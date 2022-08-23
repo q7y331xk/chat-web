14e450ce113ce5722d33a6ed3c79a7b7783f7698
dulee <dulee.dev@gmail.com>
2022-08-23 23:43:28 +0900

    FEAC

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,17 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react'
 import { io } from 'socket.io-client'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-  const socket = io('http://localhost:3201/chat', { transports: ['websocket']});
+  const socket = io('http://localhost:3201/chat',{ transports: ['websocket'] });
   socket.on('connect', () => {
     console.log('Connected');
   });
-  socket.emit('startChat', { chat: { id: 1, me: 'dulee', opponent: 'eunJi '} })
-  socket.on('disconnect', function() {
-    console.log('Disconnected');
-  });
-  socket.on('startChat', function(data) {
-    console.log('Disconnected');
-    console.log(data);
-  });
   const sendChat = () =>  {
-    socket.emit('clientToServer', { test: 'test' }, response => {
-      console.log(response);
-    });
+    socket.emit('clientToServer', { chatRoomId: 1, writer: 'dulee', createdAt: new Date(), message: "my first chat message" });
   }
   const who = () =>  {
-    socket.emit('whoAreThere', { test: 'test' }, response => {
-      console.log(response);
-    });
+    socket.emit('whoAreThere', { test: 'test' });
   }
   return (
     <div className={styles.container}>
